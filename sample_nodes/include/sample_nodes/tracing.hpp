524c43eefa9ac80d1050d19b07a3e519100ebf53
dskkato <kato.d@t2.auto>
2025-11-25 23:51:12 +0900

    Enhance tracing implementation with detailed comments for clarity

--- sample_nodes/include/sample_nodes/tracing.hpp
+++ sample_nodes/include/sample_nodes/tracing.hpp
@@ -12,68 +12,115 @@
 
 #include <lttng/tracepoint.h>
 
-TRACEPOINT_EVENT(sample_nodes, slot_selection,
-                 TP_ARGS(int32_t, slot_id, uint64_t, generation, uint64_t,
-                         byte_size, int32_t, device_id, int32_t, result),
-                 TP_FIELDS(ctf_integer(int32_t, slot_id, slot_id) ctf_integer(
-                     uint64_t, generation, generation)
-                               ctf_integer(uint64_t, byte_size, byte_size)
-                                   ctf_integer(int32_t, device_id, device_id)
-                                       ctf_integer(int32_t, result, result)))
+TRACEPOINT_EVENT(
+    /* Tracepoint provide name */
+    sample_nodes,
+    /* Tracepoint name */
+    slot_selection,
+    /* List of tracepoint arguments (input) */
+    TP_ARGS(int32_t, slot_id, uint64_t, generation, uint64_t, byte_size,
+            int32_t, device_id, int32_t, result),
+    /* List of fields of eventual event (output) */
+    TP_FIELDS(ctf_integer(int32_t, slot_id, slot_id)         //
+              ctf_integer(uint64_t, generation, generation)  //
+              ctf_integer(uint64_t, byte_size, byte_size)    //
+              ctf_integer(int32_t, device_id, device_id)     //
+              ctf_integer(int32_t, result, result))          //
+)
 
-TRACEPOINT_EVENT(sample_nodes, generation_bump,
-                 TP_ARGS(int32_t, slot_id, uint64_t, generation, uint64_t,
-                         byte_size, int32_t, device_id, int32_t, result),
-                 TP_FIELDS(ctf_integer(int32_t, slot_id, slot_id) ctf_integer(
-                     uint64_t, generation, generation)
-                               ctf_integer(uint64_t, byte_size, byte_size)
-                                   ctf_integer(int32_t, device_id, device_id)
-                                       ctf_integer(int32_t, result, result)))
+TRACEPOINT_EVENT(
+    /* Tracepoint provide name */
+    sample_nodes,
+    /* Tracepoint name */
+    generation_bump,
+    /* List of tracepoint arguments (input) */
+    TP_ARGS(int32_t, slot_id, uint64_t, generation, uint64_t, byte_size,
+            int32_t, device_id, int32_t, result),
+    /* List of fields of eventual event (output) */
+    TP_FIELDS(                                         //
+        ctf_integer(int32_t, slot_id, slot_id)         //
+        ctf_integer(uint64_t, generation, generation)  //
+        ctf_integer(uint64_t, byte_size, byte_size)    //
+        ctf_integer(int32_t, device_id, device_id)     //
+        ctf_integer(int32_t, result, result))          //
+)
 
-TRACEPOINT_EVENT(sample_nodes, cuda_memset_start,
-                 TP_ARGS(int32_t, slot_id, uint64_t, generation, uint64_t,
-                         byte_size, int32_t, device_id),
-                 TP_FIELDS(ctf_integer(int32_t, slot_id, slot_id)
-                               ctf_integer(uint64_t, generation, generation)
-                                   ctf_integer(uint64_t, byte_size, byte_size)
-                                       ctf_integer(int32_t, device_id,
-                                                   device_id)))
+TRACEPOINT_EVENT(
+    /* Tracepoint provide name */
+    sample_nodes,
+    /* Tracepoint name */
+    cuda_memset_start,
+    /* List of tracepoint arguments (input) */
+    TP_ARGS(int32_t, slot_id, uint64_t, generation, uint64_t, byte_size,
+            int32_t, device_id),
+    /* List of fields of eventual event (output) */
+    TP_FIELDS(ctf_integer(int32_t, slot_id, slot_id)         //
+              ctf_integer(uint64_t, generation, generation)  //
+              ctf_integer(uint64_t, byte_size, byte_size)    //
+              ctf_integer(int32_t, device_id, device_id))    //
+)
 
-TRACEPOINT_EVENT(sample_nodes, cuda_memset_stop,
-                 TP_ARGS(int32_t, slot_id, uint64_t, generation, uint64_t,
-                         byte_size, int32_t, device_id, int32_t, result),
-                 TP_FIELDS(ctf_integer(int32_t, slot_id, slot_id) ctf_integer(
-                     uint64_t, generation, generation)
-                               ctf_integer(uint64_t, byte_size, byte_size)
-                                   ctf_integer(int32_t, device_id, device_id)
-                                       ctf_integer(int32_t, result, result)))
+TRACEPOINT_EVENT(
+    /* Tracepoint provide name */
+    sample_nodes,
+    /* Tracepoint name */
+    cuda_memset_stop,
+    /* List of tracepoint arguments (input) */
+    TP_ARGS(int32_t, slot_id, uint64_t, generation, uint64_t, byte_size,
+            int32_t, device_id, int32_t, result),
+    /* List of fields of eventual event (output) */
+    TP_FIELDS(ctf_integer(int32_t, slot_id, slot_id)         //
+              ctf_integer(uint64_t, generation, generation)  //
+              ctf_integer(uint64_t, byte_size, byte_size)    //
+              ctf_integer(int32_t, device_id, device_id)     //
+              ctf_integer(int32_t, result, result))          //
+)
 
-TRACEPOINT_EVENT(sample_nodes, event_record,
-                 TP_ARGS(int32_t, slot_id, uint64_t, generation, uint64_t,
-                         byte_size, int32_t, device_id, int32_t, result),
-                 TP_FIELDS(ctf_integer(int32_t, slot_id, slot_id) ctf_integer(
-                     uint64_t, generation, generation)
-                               ctf_integer(uint64_t, byte_size, byte_size)
-                                   ctf_integer(int32_t, device_id, device_id)
-                                       ctf_integer(int32_t, result, result)))
+TRACEPOINT_EVENT(
+    /* Tracepoint provide name */
+    sample_nodes,
+    /* Tracepoint name */
+    event_record,
+    /* List of tracepoint arguments (input) */
+    TP_ARGS(int32_t, slot_id, uint64_t, generation, uint64_t, byte_size,
+            int32_t, device_id, int32_t, result),
+    /* List of fields of eventual event (output) */
+    TP_FIELDS(ctf_integer(int32_t, slot_id, slot_id)         //
+              ctf_integer(uint64_t, generation, generation)  //
+              ctf_integer(uint64_t, byte_size, byte_size)    //
+              ctf_integer(int32_t, device_id, device_id)     //
+              ctf_integer(int32_t, result, result))          //
+)
 
-TRACEPOINT_EVENT(sample_nodes, stream_sync,
-                 TP_ARGS(int32_t, slot_id, uint64_t, generation, uint64_t,
-                         byte_size, int32_t, device_id, int32_t, result),
-                 TP_FIELDS(ctf_integer(int32_t, slot_id, slot_id) ctf_integer(
-                     uint64_t, generation, generation)
-                               ctf_integer(uint64_t, byte_size, byte_size)
-                                   ctf_integer(int32_t, device_id, device_id)
-                                       ctf_integer(int32_t, result, result)))
-
-TRACEPOINT_EVENT(sample_nodes, message_publication,
-                 TP_ARGS(int32_t, slot_id, uint64_t, generation, uint64_t,
-                         byte_size, int32_t, device_id),
-                 TP_FIELDS(ctf_integer(int32_t, slot_id, slot_id)
-                               ctf_integer(uint64_t, generation, generation)
-                                   ctf_integer(uint64_t, byte_size, byte_size)
-                                       ctf_integer(int32_t, device_id,
-                                                   device_id)))
+TRACEPOINT_EVENT(
+    /* Tracepoint provide name */
+    sample_nodes,
+    /* Tracepoint name */
+    stream_sync,
+    /* List of tracepoint arguments (input) */
+    TP_ARGS(int32_t, slot_id, uint64_t, generation, uint64_t, byte_size,
+            int32_t, device_id, int32_t, result),
+    /* List of fields of eventual event (output) */
+    TP_FIELDS(ctf_integer(int32_t, slot_id, slot_id)         //
+              ctf_integer(uint64_t, generation, generation)  //
+              ctf_integer(uint64_t, byte_size, byte_size)    //
+              ctf_integer(int32_t, device_id, device_id)     //
+              ctf_integer(int32_t, result, result))          //
+)
+TRACEPOINT_EVENT(
+    /* Tracepoint provide name */
+    sample_nodes,
+    /* Tracepoint name */
+    message_publication,
+    /* List of tracepoint arguments (input) */
+    TP_ARGS(int32_t, slot_id, uint64_t, generation, uint64_t, byte_size,
+            int32_t, device_id),
+    /* List of fields of eventual event (output) */
+    TP_FIELDS(ctf_integer(int32_t, slot_id, slot_id)         //
+              ctf_integer(uint64_t, generation, generation)  //
+              ctf_integer(uint64_t, byte_size, byte_size)    //
+              ctf_integer(int32_t, device_id, device_id))    //
+)
 
 #endif  // defined(SAMPLE_NODES_ENABLE_TRACING) &&
         // (!defined(_SAMPLE_NODES_TRACEPOINTS_H) ||
