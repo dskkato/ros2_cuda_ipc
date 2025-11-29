0b6acda21f4f2f9b1ba5f906073115d2a89b39cf
dskkato <kato.d@t2.auto>
2025-11-29 16:51:56 +0000

    Fix include order for tracing header in conditional compilation

--- sample_nodes/src/tracing.cpp
+++ sample_nodes/src/tracing.cpp
@@ -1,7 +1,5 @@
-#include "sample_nodes/tracing.hpp"
-
 #if defined(SAMPLE_NODES_ENABLE_TRACING)
 #define TRACEPOINT_CREATE_PROBES
 #define TRACEPOINT_DEFINE
-#include "sample_nodes/tracing.hpp"
 #endif
+#include "sample_nodes/tracing.hpp"
